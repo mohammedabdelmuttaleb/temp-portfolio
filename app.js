88543b24c2648ab223ce749bc7152580dfd6cc01
mohammed abdelmuttaleb <168122825+mohammedabdelmuttaleb@users.noreply.github.com>
2025-07-31 20:46:42 +0300

    app.js

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      target.scrollIntoView({
+        behavior: "smooth"
+      });
+    }
+  });
+});
+
+
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
