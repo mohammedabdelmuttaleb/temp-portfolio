bfe80346cdd9c9a361e23755990dfd43279658fa
mohammed abdelmuttaleb <168122825+mohammedabdelmuttaleb@users.noreply.github.com>
2025-07-31 20:17:26 +0300

    app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-// Smooth scroll
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
   });
+});
+
+document.querySelectorAll('.section').forEach((section) => {
+  observer.observe(section);
 });
